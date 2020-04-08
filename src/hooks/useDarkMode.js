05173057fe7b95b1cd01a2e4b747164075469ccc
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-04-08 19:41:37 -0400

    mvp done

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,25 @@
+  
+import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
+
+export const useDarkMode = (key, initialValue) => {
+
+    const [ darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+
+    const changeDarkMode = event => {
+        event.preventDefault();
+        return darkMode ? setDarkMode(false) : setDarkMode(true);
+    }
+
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [ darkMode ]);
+
+    return [ darkMode, setDarkMode, changeDarkMode ]
+};
+
+export default useDarkMode;
